9aac452a2a413fc69ea0eb4732cbb64531b81f9b
pr07pr07 <67961663+pr07pr07@users.noreply.github.com>
2023-08-09 23:00:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    version="0.0"
+    version="0.1"
     namespace = os.getenv('POD_NAMESPACE', default = 'ns-red')
-    return {"Message": "with Love from OCI Devops ","Version":version,"Namespace":namespace}
+    return {"Message": "with Love from OCI Devops Team ","Version":version,"Namespace":namespace}
